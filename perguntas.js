7bba4345ba6ca3cb8c99a2c0346d834e909d5aad
Natan-Schumacker <160528091+Natan-Schumacker@users.noreply.github.com>
2024-08-07 09:50:20 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -5,17 +5,14 @@ export const perguntas = [
             {
                 texto: "Isso é assustador!",
                 afirmacao: [
-                    "O novo representante da nação mais desenvolvida do mundo, temeu no começo de seu mandato sobre as questões climáticas,",
-                    "",
-
+                    "você o novo representante da nação mais desenvolvida do mundo, temeu no começo de seu mandato sobre as questões climáticas,",
                 ],
                 proxima: 1,
             },
             {
                 texto: "Isso é maravilhoso!",
                 afirmacao: [
-                    "",
-                    "O novo líder da maior nação do mundo começou seu mandato entusiasmado para resolver os problemas climáticos pendentes do país",
+                    "O novo líder, você, da maior nação do mundo começou seu mandato entusiasmado para resolver os problemas climáticos pendentes do país",
                 ],
                 proxima: 1,
             },
@@ -28,8 +25,6 @@ export const perguntas = [
                 texto: "Diminuir a emissão de gases por meio de leis e fiscalização pesada ao custo de diminuir a produção industrial",
                 afirmacao: [
                     "sua primeira ação foi a criação de políticas efetivas para resolver o problema, mas deixou o preço médio dos produtos e serviços do país mais caros diminuindo a circulação de dinheiro junto a economia",
-                    "",
-                    ""
                 ],
                 proxima: 2,
             },
@@ -37,8 +32,6 @@ export const perguntas = [
                 texto: "Dar prioridade para indústria e comércio e fazer somente o que pode ser feito sem diminuir o crescimento da economia do país de forma significativa.",
                 afirmacao: [
                     "mas ele não quis atrapalhar a economia, agindo apenas de formas efetivas monetariamente, com o ônus de previsões de pouco resultado para a resolução do problema",
-                    "",
-                    ""
                 ],
                 proxima: 2,
             },
@@ -46,18 +39,17 @@ export const perguntas = [
     },
     {
     
-        enunciado: "Você é chamado alguns meses depois de implementar suas poilíticas para uma convenção sobre mudanças climáticas, eles perguntam e cobram sobre a situação, mas exclusivamente na parte de como os empresários entraram no projeto do governo sobre Sustentabilidade ambiental",
+        enunciado: "você é chamado alguns meses depois de implementar suas poilíticas para uma convenção sobre mudanças climáticas, eles perguntam e cobram sobre a situação, mas exclusivamente na parte de como os empresários entraram no projeto do governo sobre Sustentabilidade ambiental",
         alternativas: [
         {
             texto: "você fala sobre políticas que serão implantadas nas empresas como por exemplo: obrigação legislativa para empresas doarem uma porcentagem de seu terreno industrial/comercial para replantio de vegetação nativa",
             afirmacao: [
                 "e na hora para se pronunciar sobre o assunto disse que estava trabalhando arduamente para acabar com o problema.",
-                ""
             ],
             proxima: 3,
         },
         {
-            texto: "Você fala somente que eles seram mais cobrados sobre as legislações atuais, pois mais políticas seriam prejudiais a economia e iriam diminuir a capacidade de crescimento social e econômico da população",
+            texto: "você fala somente que eles seram mais cobrados sobre as legislações atuais, pois mais políticas seriam prejudiais a economia e iriam diminuir a capacidade de crescimento social e econômico da população",
             afirmacao: [
                 "e na hora para se pronunciar sobre o assunto disse que estava fazendo o possível para resolver o problema."
             ],
@@ -71,9 +63,7 @@ export const perguntas = [
             {
                 texto: "Diminuir a poluição",
                 afirmacao: [
-                    "Depois de repensar sobre suas políticas ambientais no começo de seu segundo mandato ele decidiu retomar com as políticas decisivas que fizeram o país diminuir consideravelmente as emições de poluentes, que no futuro se tornaram referência no mundo inteiro e os outros países que ignoraram essas questões sofrem por causa do ar, águas e terras contaminadas",
-                    "",
-                    ""
+                    "Depois de repensar sobre suas políticas ambientais no começo de seu segundo mandato você decidiu retomar com as políticas decisivas que fizeram o país diminuir consideravelmente as emições de poluentes, que no futuro se tornaram referência no mundo inteiro e os outros países que ignoraram essas questões sofrem por causa do ar, águas e terras contaminadas",
                 ],
                 proxima: 4,
             },
@@ -81,8 +71,6 @@ export const perguntas = [
                 texto: "Diminuir pobreza",
                 afirmacao: [
                     "Depois de repensar suas políticas para seu segundo mandato você decide focar na economia, mais especificamente no fim  da pobreza, apesar de um sucesso grandioso, as pesoas agora sofrem diariamente com a poluição diminuindo a expecttativa de vida da população, principalmente daqueles poucos que ainda continuam pobres",
-                    "",
-                    ""
                 ],
                 proxima: 4,
             },
